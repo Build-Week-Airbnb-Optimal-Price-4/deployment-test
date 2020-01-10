6796a1a0cfb5cab5f9780bf4bf25437bd21adf67
JeanFraga <JeanFraga@users.noreply.github.com>
2020-01-09 21:59:52 -0500

    fixed once again

--- airbnb_api/application.py
+++ airbnb_api/application.py
@@ -77,7 +77,7 @@ def create_app():
             f.seek(-2, os.SEEK_END)     # Jump to the second last byte.
             while f.read(1) != b"\n":   # Until EOL is found...
                 f.seek(-2, os.SEEK_CUR) # ...jump back the read byte plus one more.
-            last = f.readline()
+            last = f.readline()         # Read last line.
 
 
         print(prediction_text)
